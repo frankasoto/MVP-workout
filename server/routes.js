70846813c18163d85c6b63a23f7c9b4746ef7352
Frank Soto <fasfranksoto@gmail.com>
2023-09-13 13:25:53 -0700

    displays dates and fetches data from clicked date

--- server/routes.js
+++ server/routes.js
@@ -10,5 +10,7 @@ const router = express.Router();
 
 router.get('/exercises', controllers.getExerciseList);
 router.post('/exercises',controllers.submitWorkout);
+router.get('/exercises/dates', controllers.getDates);
+router.get('/exercises/dates/:date', controllers.getWorkoutData);
 
 module.exports = router;
